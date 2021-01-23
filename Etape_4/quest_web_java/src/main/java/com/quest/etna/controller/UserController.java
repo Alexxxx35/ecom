cd5af57272a39b645813203f32bef6c4c9e6ad90
boury_a <boury_a@etna-alternance.net>
2021-01-23 20:26:06 +0100

    step04 tmp

---
+++ Etape_4/quest_web_java/src/main/java/com/quest/etna/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.quest.etna.controller;
+
+import com.quest.etna.model.Address;
+import com.quest.etna.model.User;
+import com.quest.etna.repositories.AddressRepository;
+import com.quest.etna.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping(value = "/user/{id}")
+    public Optional<User> findById(@PathVariable Integer id) {
+        return userRepository.findById(id);
+    }
+
+    @GetMapping(value = "/users/")
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @PutMapping(value = "/user{id}", produces = "application/json")
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+
+    }
+}
--- Etape_4/quest_web_java/src/main/java/com/quest/etna/repositories/UserRepository.java
+++ Etape_4/quest_web_java/src/main/java/com/quest/etna/repositories/UserRepository.java
@@ -4,12 +4,13 @@ import com.quest.etna.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
     User findByUsername(String username);
-
+    List<User> findAll();
     Optional<User> findById(Integer id);
     //Optional<User> findByAddress(String address);
     boolean existsByUsername(String username);
